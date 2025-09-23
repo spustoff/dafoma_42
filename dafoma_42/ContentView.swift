1a77c7d128751a7378ab0dd73d242d31ac9193cd
spustoff <vyacheslavpustov05@gmail.com>
2025-09-23 18:16:25 +0300

    1.3

--- dafoma_42/ContentView.swift
+++ dafoma_42/ContentView.swift
@@ -55,7 +55,7 @@ struct ContentView: View {
     
     private func check_data() {
         
-        let lastDate = "30.09.2025"
+        let lastDate = "27.09.2025"
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy"
